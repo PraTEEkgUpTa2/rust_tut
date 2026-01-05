5ee9edb173f1e55a87ffb1897d92e852edf86b3a
Prateek Gupta <prateekgupta23090@gmail.com>
2026-01-05 18:03:08 +0000

    serde, borsh and lifetimes

--- src/main.rs
+++ src/main.rs
@@ -140,78 +140,140 @@
 
 
 
-use chrono::prelude::*;
-use dotenv::dotenv;
-use std::env;
+// use chrono::prelude::*;
+// use dotenv::dotenv;
+// use std::env;
 
 
-fn main() {
-    dotenv().ok();
-    let var = env::var("REDIS_ADD");
+// fn main() {
+//     dotenv().ok();
+//     let var = env::var("REDIS_ADD");
 
-    match var {
-        Ok(val) => println!("REDIS_ADD: {}", val),
-        Err(e) => println!("Couldn't read REDIS_ADD ({})", e),
-    }
+//     match var {
+//         Ok(val) => println!("REDIS_ADD: {}", val),
+//         Err(e) => println!("Couldn't read REDIS_ADD ({})", e),
+//     }
 
-    let utc = Utc::now();
-    println!("Current UTC time: {}", utc);
+//     let utc = Utc::now();
+//     println!("Current UTC time: {}", utc);
 
-    // Generics in rust
-    // Generics allow you to write code that can work with different data types while ensuring type safety at compile time.
+//     // Generics in rust
+//     // Generics allow you to write code that can work with different data types while ensuring type safety at compile time.
 
-    println!("Sum : {}", sum_with_bounds(5,10));
+//     println!("Sum : {}", sum_with_bounds(5,10));
 
-    let r = Rect { 
-        width: 10.0,
-        length: 20.0
-    };
+//     let r = Rect { 
+//         width: 10.0,
+//         length: 20.0
+//     };
 
-    print_area_shape(r);
+//     print_area_shape(r);
 
-    // println!("Area of rectangle: {}", r.area());
-}
+//     // println!("Area of rectangle: {}", r.area());
+// }
 
-// fn sum<T>(a: T, b: T) -> T {
+// // fn sum<T>(a: T, b: T) -> T {
+// //     return a + b;
+// // } 
+// // this will throw an error as + operator is not defined for generic type T for removing this error we can use trait bounds
+
+// fn sum_with_bounds<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
 //     return a + b;
-// } 
-// this will throw an error as + operator is not defined for generic type T for removing this error we can use trait bounds
-
-fn sum_with_bounds<T: std::ops::Add<Output = T>>(a: T, b: T) -> T {
-    return a + b;
-}
+// }
 
 
-// Generics with structs
+// // Generics with structs
 
 
-trait Shape {
-    fn area(&self) -> f32;
+// trait Shape {
+//     fn area(&self) -> f32;
 
-}
-struct Rect {
-    width: f32,
-    length: f32
-}
+// }
+// struct Rect {
+//     width: f32,
+//     length: f32
+// }
 
-// impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
-//     fn area(&self) -> T {
+// // impl<T: std::ops::Mul<Output = T> + Copy> Rect<T> {
+// //     fn area(&self) -> T {
+// //         return self.width * self.length;
+// //     }
+// // }
+
+// // trait
+
+
+
+// // whoever implements this trait has to define area function
+
+// impl Shape for Rect {
+//     fn area(&self) -> f32 {
 //         return self.width * self.length;
 //     }
 // }
 
-// trait
+// fn print_area_shape<T: Shape>(s: T) {
+//     println!("Area: {}", s.area());
+// }
 
+// SERDE in rust
 
+// use serde::{Serialize, Deserialize};
+use borsh::{BorshDeserialize, BorshSerialize};
 
-// whoever implements this trait has to define area function
+#[derive(BorshDeserialize, BorshSerialize)]
 
-impl Shape for Rect {
-    fn area(&self) -> f32 {
-        return self.width * self.length;
-    }
+struct User {
+    username: String,
+    password: String,
 }
 
-fn print_area_shape<T: Shape>(s: T) {
-    println!("Area: {}", s.area());
+fn main(){
+    let u = User {
+        username: String::from("alice"),
+        password: String::from("password123"),
+    };
+
+    let mut v: Vec<u8> = Vec::new();
+
+    //Convert struct to bytes and store in vector v
+    let ans = u.serialize((&mut v));
+
+    let user = User::try_from_slice(&v).unwrap();
+
+    println!("Username: {}, Password: {}", user.username, user.password);
+
+    // let serialized = serde_json::to_string(&u);
+
+    // match serialized {
+    //     Ok(s) => println!("Serialized User: {}", s),
+    //     Err(e) => println!("Error serializing user: {}", e),
+    // }
+
+    // Lifetimes in rust
+
+    let str1 = String::from("Hello, world!");
+
+    let ans;
+    {
+        let str2 = String::from("Hello, Rust!");
+        ans = longest(&str1, &str2);
+        println!("Longest string: {}", ans);
+    }
+
+    
+    // now this will throw an error as str2 goes out of scope and so prevent this we use lifetimes
+
+    
+}
+
+fn longest<'a>(s1: &'a String, s2: &'a String) -> &'a String {
+    if s1.len() > s2.len() {
+        return s1;
+    } else {
+        return s2;
+        
+    }
+
+    // this will always return lowest lifetime b/w s1 and s2
 }
